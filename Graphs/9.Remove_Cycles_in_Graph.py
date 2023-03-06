e0c15dcfcf3a00351a7be44c26b7ba174121aec1
ventsigachev <vgachev@gmail.com>
2023-03-06 13:43:11 +0200

    Problem description

---
+++ Graphs/9.Remove_Cycles_in_Graph.py
@@ -0,0 +1,45 @@
+"""
+    You are given an undirected multi-graph. Remove a minimal number of edges to make the graph acyclic (to break all
+    cycles). As a result, print the number of edges removed and the removed edges. If several edges can be removed to
+    break a certain cycle, remove the smallest of them in ascendant order.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+Possible inputs:
+
+8
+1 -> 2 5 4
+2 -> 1 3
+3 -> 2 5
+4 -> 1
+5 -> 1 3
+6 -> 7 8
+7 -> 6 8
+8 -> 6 7
+
+14
+K -> X J
+J -> K N
+N -> J X L M
+X -> K N Y
+M -> N I
+Y -> X L
+L -> N I Y
+I -> M L
+A -> Z Z Z
+Z -> A A A
+F -> E B P
+E -> F P
+P -> B F E
+B -> F P
+
+"""
