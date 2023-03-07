d90f7ef6b38fce2501c19eb9fc3acbe308827df4
ventsigachev <vgachev@gmail.com>
2023-03-07 17:39:25 +0200

    Task descrription

---
+++ Graphs/10.Find_Important_Edges.py
@@ -0,0 +1,38 @@
+"""
+    The purpose of the task is to find the edges, without them a graph becomes a multi-graph.
+    On the first line of input one will receive the number of nodes,
+    on the second line - number of edges, and on the next N lines connected nodes.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+    
+5
+5
+1 - 0
+0 - 2
+2 - 1
+0 - 3
+3 - 4
+
+7
+8
+0 - 1
+1 - 2
+2 - 0
+1 - 3
+1 - 4
+1 - 6
+3 - 5
+4 - 5
+
+"""
