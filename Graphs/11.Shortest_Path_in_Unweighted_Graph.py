2f98187f7e4f9e1a66434115378f2145a9e6d73f
ventsigachev <vgachev@gmail.com>
2023-03-08 17:51:12 +0200

    Task description

---
+++ Graphs/11.Shortest_Path_in_Unweighted_Graph.py
@@ -0,0 +1,52 @@
+"""
+    You will be given a graph from the console.
+    Your task is to find the shortest path and print it back on the console.
+    The first line will be the number of Nodes - N the second one the number of Edges - E,
+    then on each E line the edge in form {destination} – {source}.
+    On the last two lines, you will read the start node and the end node.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+Possible Inputs:
+
+8
+10
+1 2
+1 4
+2 3
+4 5
+5 8
+5 6
+5 7
+5 8
+6 7
+7 8
+1
+7
+
+11
+11
+1 5
+1 4
+5 7
+7 8
+8 2
+2 3
+3 4
+4 1
+6 2
+9 10
+11 9
+6
+3
+
+"""
