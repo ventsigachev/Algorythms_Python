341dbdc3b3da527ea1c83d4341618e581e1697fb
ventsigachev <vgachev@gmail.com>
2023-03-09 22:24:37 +0200

    Task description

---
+++ Graphs/12.Dijkstra's_Algorithm.py
@@ -0,0 +1,84 @@
+"""
+    Dijkstra's algorithm finds the shortest path from given vertex to all other vertices in a directed / undirected
+    weighted graph. First described by Edsger W. Dijkstra in 1956. Assumptions:
+        ▪ Weights on edges are non-negative, works only with positive numbers.
+        ▪ Edges can be directed or not.
+        ▪ Weights do not have to be distances.
+        ▪ The shortest path is not necessarily unique.
+        ▪ Not all edges need to be reachable.
+        ▪ Uses priority queue instead of queue.
+
+    Task Description
+        Find the shortest path between two nodes in a weighted graph.
+        Dijkstra’s algorithm is one of the most famous ones that solve this task, by implementing the optimized
+        version of Dijkstra’s algorithm using a priority queue.
+
+        Input
+            • On the first, you will receive an integer – e – number of edges.
+            • On the next e lines, you will receive an edge in the following format: "{start}, {end}, {weight}".
+            • On the next line, you will receive a start node.
+            • On the last line, you will receive an end node.
+        Output
+            • Print the cost of the shortest path.
+            • Print all nodes that form the shortest path.
+            • If the end node can’t be reached from the start node, then you have to print: "There is no such path."
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs
+    
+18
+0, 6, 10
+0, 8, 12
+6, 4, 17
+6, 5, 6
+8, 5, 3
+5, 4, 5
+5, 11, 33
+8, 2, 14
+2, 11, 9
+2, 7, 15
+4, 1, 20
+4, 11, 11
+11, 1, 6
+11, 7, 20
+1, 9, 5
+1, 7, 26
+7, 9, 3
+3, 10, 7
+0
+9
+
+18
+0, 6, 10
+0, 8, 12
+6, 4, 17
+6, 5, 6
+8, 5, 3
+5, 4, 5
+5, 11, 33
+8, 2, 14
+2, 11, 9
+2, 7, 15
+4, 1, 20
+4, 11, 11
+11, 1, 6
+11, 7, 20
+1, 9, 5
+1, 7, 26
+7, 9, 3
+3, 10, 7
+0
+10
+
+"""
