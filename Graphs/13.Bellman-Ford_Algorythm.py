44154a31982dea35a3e067b528c9c33598f75b02
ventsigachev <vgachev@gmail.com>
2023-03-11 15:37:26 +0200

    Task description

---
+++ Graphs/13.Bellman-Ford_Algorythm.py
@@ -0,0 +1,43 @@
+"""
+    Finds the shortest path in weighted graph, with negative edges.
+        ▪ Computes the shortest paths from a source vertex to all other vertices in a weighted digraph
+        ▪ Named after Richard Bellman and Lester Ford Jr., who published it in 1958 and 1956, respectively
+        ▪ Can detect and report negative cycles
+        ▪ Time complexity: O(VE)
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+
+6
+8
+1 2 8
+1 3 10
+2 4 1
+3 6 2
+4 3 -4
+4 6 -1
+6 5 -2
+5 3 1
+1
+6
+
+4
+4
+1 2 1
+2 3 -1
+3 4 -1
+4 1 -1
+1
+4
+
+"""
