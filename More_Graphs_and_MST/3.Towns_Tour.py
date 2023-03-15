ae497b0605baea30928e467d1ae582ec6508982d
ventsigachev <vgachev@gmail.com>
2023-03-15 11:03:32 +0200

    Task description

---
+++ More_Graphs_and_MST/3.Towns_Tour.py
@@ -0,0 +1,30 @@
+"""
+    Visit every node in the graph as cheap as possible.
+
+    Input
+        • On the first line, you will be given the number of the nodes.
+        • On the second line, you will be given the number of edges (n).
+        • On the next n lines, you will be given a connection in the format: "{first} - {second} - {cost}".
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+    
+4
+5
+0 - 1 - 10
+0 - 2 - 6
+0 - 3 - 5
+1 - 3 - 15
+2 - 3 - 4
+
+"""
