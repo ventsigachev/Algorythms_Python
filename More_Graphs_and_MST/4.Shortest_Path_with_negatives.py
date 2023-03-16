72892ce1526572e831f930aa09b1a03b57cf5e7e
ventsigachev <vgachev@gmail.com>
2023-03-16 20:39:07 +0200

    Task description

---
+++ More_Graphs_and_MST/4.Shortest_Path_with_negatives.py
@@ -0,0 +1,51 @@
+"""
+   The task is to find the shortest path in a graph from S vertex to D vertex.
+   However, edges might have negative weights.
+   Input
+    • On the first line, you will receive an integer – n – number of nodes.
+    • On the second line, you will receive an integer – e – number of edges.
+    • On the next e lines, you will receive edges in the following format: "{source} {destination} {weight}".
+    • On the next line, you will receive an integer – source – the start of the path.
+    • On the last line, you will receive an integer – destination – end of the path.
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+ 
+5
+8
+1 2 -1
+1 3 4
+2 3 3
+2 4 2
+2 5 2
+4 2 1
+4 3 5
+5 4 -3
+1
+4
+
+5
+8
+1 2 -1
+1 3 4
+2 3 3
+2 4 2
+2 5 2
+4 2 -1
+4 3 5
+5 4 -3
+1
+4
+
+"""
