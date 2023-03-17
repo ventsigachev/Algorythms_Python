ecbefb2690631a43c59b1ebb1bda694b4f7f9ca3
ventsigachev <vgachev@gmail.com>
2023-03-17 18:10:04 +0200

    Task description

---
+++ More_Graphs_and_MST/5.Cable_Network.py
@@ -0,0 +1,77 @@
+"""
+    A cable networking company plans to extend its existing cable network by connecting as many customers as possible,
+    within a fixed budget limit. The company has calculated the cost of building some prospective connections.
+    You are given the existing cable network (a set of customers and connections between them), along with the estimated
+    connection costs between some pairs of customers and prospective customers. A customer can only be connected to the
+    network, via a direct connection, with an already connected customer.
+
+    The task is to connect as many customers as possible, within the existing budget.
+
+    Input
+        • On the first line, you will receive an integer – budget.
+        • On the second line, you will receive an integer – n – number of nodes.
+        • On the third line, you will receive an integer – e – number of edges.
+        • On the next e lines, you will receive edges in the following format: "{first} {second} {weight} {connected}".
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+
+20
+9
+15
+1 4 8
+4 0 6 connected
+1 7 7
+4 7 10
+4 8 3
+7 8 4
+0 8 5 connected
+8 6 9
+8 3 20 connected
+0 5 4
+0 3 9 connected
+6 3 8
+6 2 12
+5 3 2
+3 2 14 connected
+
+7
+4
+5
+0 1 9
+0 3 4 connected
+3 1 6
+3 2 11 connected
+1 2 5
+
+20
+8
+16
+0 1 4
+0 2 5
+0 3 1 connected
+1 2 8
+1 3 2
+2 3 3
+2 4 16
+2 5 9
+3 4 7
+3 5 14
+4 5 12
+4 6 22
+4 7 9
+5 6 6
+5 7 18
+6 7 15
+
+"""
