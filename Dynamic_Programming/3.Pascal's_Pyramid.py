be3307ce29a5b384832b494083d8a3d83420d319
ventsigachev <vgachev@gmail.com>
2023-03-23 17:02:27 +0200

    Task description

---
+++ Dynamic_Programming/3.Pascal's_Pyramid.py
@@ -0,0 +1,30 @@
+"""
+    Write a program that finds the binomial coefficient  for given non-negative integers n and k.
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+    
+3
+2
+
+4
+0
+
+6
+2
+
+49
+10
+
+"""
