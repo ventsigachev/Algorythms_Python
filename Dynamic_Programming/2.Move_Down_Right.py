cc0fa88e442bd4d6afd8cdabd555ffea7a6cdd5e
ventsigachev <vgachev@gmail.com>
2023-03-23 10:57:40 +0200

    Task description

---
+++ Dynamic_Programming/2.Move_Down_Right.py
@@ -0,0 +1,41 @@
+"""
+    Given a matrix of N by M cells filled with positive integers, find the path from top left to bottom right with the
+    highest sum of cells by moving only down or right.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+    
+3
+3
+1 1 1
+1 1 1
+1 1 1
+
+3
+3
+1 0 6
+8 3 7
+2 4 9
+
+8
+7
+2 6 1 8 9 4 2
+1 8 0 3 5 6 7
+3 4 8 7 2 1 8
+0 9 2 8 1 7 9
+2 7 1 9 7 8 2
+4 5 6 1 2 5 6
+9 3 5 2 8 1 9
+2 3 4 1 7 2 8
+
+"""
