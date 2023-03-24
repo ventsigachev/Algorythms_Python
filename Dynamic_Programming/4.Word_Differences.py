80335d2273f320801888b8683aa87bae8d331996
ventsigachev <vgachev@gmail.com>
2023-03-24 17:57:38 +0200

    Task description

---
+++ Dynamic_Programming/4.Word_Differences.py
@@ -0,0 +1,29 @@
+"""
+    Write a program that finds all the differences between two strings. You have to determine the smallest
+    set of deletions and insertions to make the first string equal to the second.
+    Finally, you have to print the count of the minimum insertions and deletions.
+        Input
+            • You will receive the two strings on separate lines
+        Output
+            • Print the minimum amount of deletions and insertions
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+
+YMCA
+HMBB
+
+JFEIOWHGOW
+GHEWQHFEWQ
+
+"""
