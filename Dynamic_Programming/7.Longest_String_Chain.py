266e5f84224f4760b06894268f30a24ed0df58e8
ventsigachev <vgachev@gmail.com>
2023-03-28 21:05:35 +0300

    Task description

---
+++ Dynamic_Programming/7.Longest_String_Chain.py
@@ -0,0 +1,26 @@
+"""
+    Given a list of strings, write a program that returns the longest string chain that can be built from those strings.
+    A string chain is defined as follows: subsequence of a given sequence in which the subsequence's elements
+    are in sorted order (string length), lowest to highest, and in which the subsequence is as long as possible.
+    If several sequences with equal length exist, find the left-most of them.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+
+a ab abcd abc
+
+a ab abcd abc abcd abcde
+
+abde abc abd abcde ade ae 1abde abcdef
+
+"""
