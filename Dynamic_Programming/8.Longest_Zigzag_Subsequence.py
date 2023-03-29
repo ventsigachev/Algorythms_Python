140c4d95a44189b69933b2502f370390ff4e3abe
ventsigachev <vgachev@gmail.com>
2023-03-29 20:55:59 +0300

    Task description

---
+++ Dynamic_Programming/8.Longest_Zigzag_Subsequence.py
@@ -0,0 +1,29 @@
+"""
+    A zigzag sequence is one that alternately increases and decreases. More formally, such a sequence has to comply
+    with one of the two rules below:
+    1) Every even element is smaller than its neighbors and every odd element is larger than its neighbors, or
+    2) Every odd element is smaller than its neighbors and every even element is larger than its neighbors
+    1 3 2 is a zigzag sequence, but 1 2 3 is not. Any sequence of one or two elements is zigzag.
+    Find the longest zigzag subsequence in a given sequence.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+    Possible Inputs:
+
+8 3 5 7 0 8 9 10 20 20 20 12 19 11
+
+1 2 3
+
+1 3 2
+
+24 5 31 3 3 342 51 114 52 55 56 58
+
+"""
