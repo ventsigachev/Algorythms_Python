70adba9615b00d935f77f4398ce11003be5b646e
ventsigachev <vgachev@gmail.com>
2023-03-30 22:12:32 +0300

    Task description

---
+++ Prep/1.The_Story_Telling.py
@@ -0,0 +1,37 @@
+"""
+    You will be given the stories in the following format on a single line until the command "End" is received.
+        {preStory} -> {postStory1} {postStory2} {postStory n-1}
+    You have to read the stories data and then print the stories in the correct order,
+    according to the description above. Keep in mind that the structure above represents relation in which any story
+    can be preStory or postStory in different input lines.
+    Input
+        •On a single line, the input in the above format until the "End" command is received.
+    Output
+        •On a single line print, the stories in the correct order separated by spaces.
+        •The output priority, if there are more than one story without pre story, should be in the order of the input.
+    Constraints
+        •All stories will be valid and also can contain any ASCII symbol except space.
+        •The input will always end with "End" line.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+    Possible Inputs:
+   
+Mort -> Time Space
+Time -> Future Robot
+Space -> Metro
+Future -> Space Metro
+Robot -> Future
+Metro ->
+End
+
+"""
