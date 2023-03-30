995d53cea8cf60f232a0c79f3eb2f3498db0ebb8
ventsigachev <vgachev@gmail.com>
2023-03-30 23:33:54 +0300

    Representing the output

--- Prep/1.The_Story_Telling.py
+++ Prep/1.The_Story_Telling.py
@@ -7,7 +7,7 @@
     Input
         •On a single line, the input in the above format until the "End" command is received.
     Output
-        •On a single line print, the stories in the correct order separated by spaces.
+        •On a single line print the stories in the correct order, separated by spaces.
         •The output priority, if there are more than one story without pre story, should be in the order of the input.
     Constraints
         •All stories will be valid and also can contain any ASCII symbol except space.
@@ -15,16 +15,22 @@
 """
 
 
-def dfs(n, g, v):
+def dfs(n, g, v, r):
 
     if n in v:
         return
 
     v.add(n)
     for c in g[n]:
-        dfs(c, g, v)
+        dfs(c, g, v, r)
 
-    print(n)
+    r.append(n)
+
+
+def printing_res(result):
+
+    print("The story order is:")
+    print(*reversed(result))
 
 
 def main():
@@ -40,8 +46,11 @@ def main():
         graph[node] = children
 
     visited = set()
+    result = []
     for node in graph:
-        dfs(node, graph, visited)
+        dfs(node, graph, visited, result)
+
+    printing_res(result)
 
 
 if __name__ == "__main__":
