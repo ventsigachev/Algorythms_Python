95ecff77d0fcf1aface3c405b4836bccd801719c
ventsigachev <vgachev@gmail.com>
2023-04-01 21:18:56 +0300

    Task description

---
+++ Prep/2.Time.py
@@ -0,0 +1,36 @@
+"""
+    You will be given two sequences of integers representing two timeline versions. You need to extract from both
+    sequences the single correct timeline that can be retrieved by finding the longest subsequence of equal integers
+    from both timelines and also finding its length.
+        Input
+        •   The first line integers represent the first timeline separated by spaces.
+        •   The second line integers represent the second timeline separated by spaces.
+
+        Output
+        •   On the first line print the correct timeline, if there are more than one optimal timeline print the rightmost.
+        •   On the second line print the length of that timeline.
+
+        Constraints
+        •   All numbers will be valid integers.
+        •   The length of the timelines will be between [1…5000].
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+    Possible Inputs:
+
+13 42 69 73 42 84 26
+13 54 73 42 8 15 29
+
+5 69 78 5 3 5 5 5
+1 2 3 5 5 5 5 5
+
+"""
